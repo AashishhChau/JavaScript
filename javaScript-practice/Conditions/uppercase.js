cecd3a15d4b6eab2c7e9bbc2189d62861ea9f415
AashishhChau <aashishchau012@gmail.com>
2026-05-13 14:33:36 +0545

    Check if a character is uppercase or lowercase.

---
+++ javaScript-practice/Conditions/uppercase.js
@@ -0,0 +1,11 @@
+//  Check Uppercase: Write an `if-else` statement to check if a character is uppercase. Print "Uppercase" 
+// if true, otherwise "Lowercase."
+
+
+let char = "A";
+
+if (char === char.toUpperCase()) {
+    console.log("Uppercase");
+} else {
+    console.log("Lowercase");
+}
