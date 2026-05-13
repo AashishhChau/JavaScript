053a41e08b612ea668824e89d80fe30d5314c82f
AashishhChau <aashishchau012@gmail.com>
2026-05-13 14:52:46 +0545

    check if the temperature is hot or cold.

---
+++ javaScript-practice/Conditions/temperature.js
@@ -0,0 +1,11 @@
+// . Temperature Check: Write an `if-else` statement to check if the temperature is above 30 degrees. Print 
+// "Hot" if true, otherwise "Cold." 
+
+
+let temp = 25;
+
+if (temp > 30) {
+    console.log("Hot");
+} else {
+    console.log("Cold");
+}
