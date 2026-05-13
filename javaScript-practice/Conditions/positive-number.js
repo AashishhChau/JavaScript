e0134623f4e7f57daf254a2fecd4afe171b1e0c5
AashishhChau <aashishchau012@gmail.com>
2026-05-13 13:28:37 +0545

    Check if a number is positive using if-else

---
+++ javaScript-practice/Conditions/positive-number.js
@@ -0,0 +1,10 @@
+// Number Sign: Write an `if-else` statement to check if a number is positive. Print "Positive" if true, 
+// otherwise "Not positive." 
+
+let num = 7;
+if(num > 0){
+    console.log("Positive");
+}else{
+    console.log("Not Positive");
+    
+}
