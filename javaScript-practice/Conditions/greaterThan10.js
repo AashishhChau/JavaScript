722e19259a5aaa9231157b84525925a8bf5745be
AashishhChau <aashishchau012@gmail.com>
2026-05-13 14:00:59 +0545

    check if a number is greater than 10.

---
+++ javaScript-practice/Conditions/greaterThan10.js
@@ -0,0 +1,11 @@
+// Greater Than Ten: Write an `if-else` statement to check if a number is greater than 10. Print "Greater 
+// than 10" if true, otherwise "10 or less." 
+
+let num = 9;
+if(num > 10){
+    console.log("Greater than 10");
+    
+}else{
+    console.log("Less than 10");
+    
+}
