0ff1c95d821de18f636251a183d4b2556f27ba04
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:11:24 +0545

    Chech if a user's password matches or not

---
+++ javaScript-practice/Conditions/password.js
@@ -0,0 +1,9 @@
+// . Password Match: Write an `if-else` statement to check if a user’s input matches the password "1234". 
+// Print "Access granted" if true, otherwise "Access denied
+
+let password = "1234";
+if(password ===1234){
+ console.log("Acess granted");
+}else{
+    console.log("Access denied");
+}
