f4e4543402475dafab09f40e3e45648659e1359c
AashishhChau <aashishchau012@gmail.com>
2026-05-13 22:36:14 +0545

    Check if a student is fail or pas

---
+++ javaScript-practice/Conditions/checkGrade.js
@@ -0,0 +1,9 @@
+//  Grade Check: Write an `if-else` statement to check if a student’s score is greater than or equal to 50. 
+// Print "Pass" if true, otherwise "Fail."
+
+let score= 40;
+if(score >=50){
+    console.log("pass");
+}else{
+    console.log("fail");
+}
