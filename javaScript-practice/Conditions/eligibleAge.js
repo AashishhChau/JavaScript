3428399658db4d6121fdf88eeb48bffb49a75933
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:31:50 +0545

    check if a person is eligible for senior discount

---
+++ javaScript-practice/Conditions/eligibleAge.js
@@ -0,0 +1,9 @@
+// Eligible for Discount: Write an `if-else` statement to check if a customer’s age is 65 or older. Print 
+// "Eligible for senior discount" if true, otherwise "Not eligible."
+
+let age = 66;
+if (age >= 65){
+    console.log("Eligible for senior discount");
+}else{
+    console.log("Not eligible");
+}
