11952974f11ac17fb6db2b919f04a5832f849203
AashishhChau <aashishchau012@gmail.com>
2026-05-13 22:55:26 +0545

    Check a person is teenager of not.

---
+++ javaScript-practice/Conditions/checkTeenager.js
@@ -0,0 +1,11 @@
+// Teenager Check: Write an `if-else` statement to check if a person’s age is between 13 and 19. Print 
+// "Teenager" if true, otherwise "Not a teenager."
+
+let age = 20;
+if(age >=13 && age <=19){
+    console.log("Teenager");
+    
+}else{
+    console.log("Not a teenager");
+    
+}
