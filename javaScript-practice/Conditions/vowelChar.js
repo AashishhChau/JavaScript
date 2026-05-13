3f51df0f75c50a97393eeffc1571cd26bf6e4f82
AashishhChau <aashishchau012@gmail.com>
2026-05-13 13:44:49 +0545

    chech if the given character is vowel or not.

---
+++ javaScript-practice/Conditions/vowelChar.js
@@ -0,0 +1,11 @@
+// . Vowel or Consonant: Write an `if-else` statement to check if a given character is a vowel. Print "Vowel" 
+// if true, otherwise "Consonant."
+
+
+let char = "H";
+if(['a','e','i','o','u','A', 'E', 'I', 'O', 'U'].includes(char)){
+    console.log("Vowel");
+}else{
+    console.log("Consonant");
+    
+}
