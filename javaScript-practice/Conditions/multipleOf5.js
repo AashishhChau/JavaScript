c3e5b1ce00c2b69c0d6048ecd4d56cad2788dcb8
AashishhChau <aashishchau012@gmail.com>
2026-05-13 13:55:59 +0545

    Check if a multiple of 5 or not

---
+++ javaScript-practice/Conditions/multipleOf5.js
@@ -0,0 +1,10 @@
+// Multiple of Five: Write an `if-else` statement to check if a number is a multiple of 5. Print "Multiple of 
+// 5" if true, otherwise "Not a multiple of 5."
+
+
+let num  = 551;
+if(num % 5 == 0){
+    console.log("Multiple of 5");
+}else{
+    console.log("Not a Multiple of 5")
+}
