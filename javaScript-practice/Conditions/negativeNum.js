fc83971c37000fe5312ac79d6bf7be20d3b1e0d4
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:07:40 +0545

    Check if a number is negative or positive

---
+++ javaScript-practice/Conditions/negativeNum.js
@@ -0,0 +1,9 @@
+// Check Negative: Write an `if-else` statement to check if a number is negative. Print "Negative" if true, 
+// otherwise "Positive or zero."
+
+let num = 7;
+if(num < 0){
+    console.log("Negative number");
+}else{
+    console.log("positive Number");
+}
