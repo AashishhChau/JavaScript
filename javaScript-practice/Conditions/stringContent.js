bd05946c61f919d0c40d4f529e530d90d49817b3
AashishhChau <aashishchau012@gmail.com>
2026-05-13 22:58:29 +0545

    Check if a string contains the letter a or not

---
+++ javaScript-practice/Conditions/stringContent.js
@@ -0,0 +1,10 @@
+// . String Content Check: Write an `if-else` statement to check if a string contains the letter "a". Print 
+// "Contains 'a'" if true, otherwise "Does not contain 'a'."
+
+let str = "Aashish";
+
+if (str.includes("a")) {
+    console.log("Contains 'a'");
+} else {
+    console.log("Does not contain 'a'");
+}
