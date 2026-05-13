0e74bcab8f5ba58e4bb21cdfde71da35d51fa93d
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:03:28 +0545

    Check if a given year is leap year or not

---
+++ javaScript-practice/Conditions/leapYear.js
@@ -0,0 +1,10 @@
+//  Leap Year Check: Write an `if-else` statement to check if a given year is a leap year. Print "Leap year" 
+// if true, otherwise "Not a leap year."
+let year =2054;
+if(year%4 ===0){
+    console.log("Leap year")
+    
+}else{
+    console.log("Not a leap year");
+    
+}
