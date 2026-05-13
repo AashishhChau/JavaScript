644b96f003a9c594f2a23003ef920dc95e6ba63a
AashishhChau <aashishchau012@gmail.com>
2026-05-13 13:10:38 +0545

    ageCheck Question and Solution

--- javaScript-practice/Conditions/ageCheck.js
+++ javaScript-practice/Conditions/ageCheck.js
@@ -1,6 +1,7 @@
 // Age Check: Write an `if-else` statement to check if a person is 18 years old or older. Print "Adult" if 
 // true, otherwise "Not an adult."
 
+
 let age =18;
 if(age >=18){
     console.log("Adult");
