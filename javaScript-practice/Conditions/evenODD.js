b86ee8ad867b5e6ef7e5d640cd92f92f76571b3d
AashishhChau <aashishchau012@gmail.com>
2026-05-13 13:33:12 +0545

    chech if a number is even or odd.

---
+++ javaScript-practice/Conditions/evenODD.js
@@ -0,0 +1,11 @@
+// . Even or Odd: Write an `if-else` statement to check if a number is even. Print "Even" if true, otherwise 
+// "Odd." 
+
+let num = 10;
+if(num%2 == 0){
+    console.log("Even");
+    
+}else{
+    console.log("Odd");
+    
+}
