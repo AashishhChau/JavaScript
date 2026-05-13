a4d3c12384bccec061f2232e4ee8a4b3fe7ce9eb
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:43:24 +0545

    checl if a number is multiple of 2 or not.

---
+++ javaScript-practice/Conditions/string.js
@@ -0,0 +1,10 @@
+// . String Comparison: Write an `if-else` statement to check if two strings are exactly the same. Print 
+// "Match" if true, otherwise "Do not match." 
+
+    let string1 = "hello";
+    let string2 = "aahish";
+    if(string1 === string2){
+        console.log("Matches");
+    }else{
+        console.log("Does not mactch");
+    }
