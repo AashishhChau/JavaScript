bceb81c3dbacf2d7dad84baeb4ed64747a29a031
AashishhChau <aashishchau012@gmail.com>
2026-05-13 14:05:53 +0545

    check if two numbers are equal of not

---
+++ javaScript-practice/Conditions/equality.js
@@ -0,0 +1,10 @@
+// Greater Than Ten: Write an `if-else` statement to check if a number is greater than 10. Print "Greater 
+// than 10" if true, otherwise "10 or less." 
+
+let num0=15;
+let num1=10;
+if(num==num1){
+    console.log("Equal");
+}else{
+    console.log("Not equal");
+}
