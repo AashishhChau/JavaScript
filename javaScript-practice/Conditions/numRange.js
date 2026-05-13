207ff81a113f550652e99e763b70355dfddc59c4
AashishhChau <aashishchau012@gmail.com>
2026-05-13 22:28:25 +0545

    Check if a number is between 1 and 100

---
+++ javaScript-practice/Conditions/numRange.js
@@ -0,0 +1,9 @@
+//  Number Range: Write an `if-else` statement to check if a number is between 1 and 100. Print "In range" if true, otherwise "Out of range.
+
+let num = 50;
+
+if (num >= 1 && num <= 100) {
+    console.log("In range");
+} else {
+    console.log("Out of range");
+}
