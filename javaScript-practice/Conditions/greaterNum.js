64a8c7e46a7f226fc8aa098b563d0ac2d15b6d0d
AashishhChau <aashishchau012@gmail.com>
2026-05-13 23:16:03 +0545

    Check if first two number is greater

---
+++ javaScript-practice/Conditions/greaterNum.js
@@ -0,0 +1,10 @@
+// Greater Number: Write an `if-else` statement to check if the first of two numbers is greater. Print 
+// "First is greater" if true, otherwise "Second is greater or equal."
+
+let num1 = 10;
+let num2 = 20;
+if(num1 > num2){
+    console.log("First number is greater");
+}else{
+    console.log("Second number is greater");
+}
