cf51f8503a7ed47c0b1cbfdd623c3f8a70f1d393
AashishhChau <aashishchau012@gmail.com>
2026-05-14 23:31:57 +0545

    All the numbers from 1 to 100 that are divisible by both 2 and 5

---
+++ javaScript-practice/Functions/divisibleOf2And5.js
@@ -0,0 +1,10 @@
+//  Write a function that uses a `for` loop to print all numbers from 1 to 100 that are divisible by both 2 and 5. 
+
+function divisible(){
+    for(let i=1; i<=100; i++){
+        if(i%2 === 0 && i%5 ===0){
+          console.log(i);
+        }
+    }
+}
+divisible();
