a6ac1e42ce27097b750261375a70fb9168a599fe
AashishhChau <aashishchau012@gmail.com>
2026-05-14 21:42:38 +0545

    Print even and odd numbers from 1 to 20

---
+++ javaScript-practice/Functions/evenNum.js
@@ -0,0 +1,10 @@
+// Write a function that uses a `for` loop to print all even numbers between 1 and 20.
+
+function eveNum(){
+    for(let i=1; i<=20; i++){
+        if(i%2 === 0){
+            console.log(i);
+        }
+    }
+}
+eveNum();
---
+++ javaScript-practice/Functions/oddNum.js
@@ -0,0 +1,9 @@
+// . Write a function that uses a `for` loop to print all odd numbers between 1 and 20.
+function oddNum(){
+    for(let i=1; i<=20; i++){
+        if(i%2 !==0){
+            console.log(i);
+        }
+    }
+}
+oddNum();
