681cd812e586ca78bc059923ed100881ceaeb6b9
AashishhChau <aashishchau012@gmail.com>
2026-05-14 21:53:10 +0545

    Multiplication of 5

---
+++ javaScript-practice/Functions/tableOf5.js
@@ -0,0 +1,9 @@
+//  Write a function that uses a `for` loop to print the multiplication table of 5. 
+
+
+function table(){
+    for(let i=1; i<=10; i++){
+        console.log(5*i);
+    }
+}
+table();
