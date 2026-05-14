50b9eb2f08ab0e82f9558cd6387450634a54356a
AashishhChau <aashishchau012@gmail.com>
2026-05-14 22:28:44 +0545

    Calculate and return the sum of odd numbers between 1 to 50.

---
+++ javaScript-practice/Functions/sumOfOddNum.js
@@ -0,0 +1,11 @@
+//  Write a function that uses a `for` loop to calculate and return the sum of all odd numbers between 1 and 50. 
+
+function sumOfOddNum(){
+    let sum=0;
+    for(let i=1; i<=50; i++){
+        if(i%2 !==0){
+            sum += i;
+        }
+    }return sum;
+}
+console.log(sumOfOddNum());
