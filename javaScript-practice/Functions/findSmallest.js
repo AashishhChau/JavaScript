412e02bd98690fe795bc379057350e851159cd7d
AashishhChau <aashishchau012@gmail.com>
2026-05-14 23:17:03 +0545

    Find and return the smallest number greater than 0 that is divisible by both 3 and 4

---
+++ javaScript-practice/Functions/findSmallest.js
@@ -0,0 +1,11 @@
+//  Write a function that uses a `for` loop to find and return the smallest number greater than 0 that is 
+// divisible by both 3 and 4. 
+
+function findSmallest(){
+    for(let i=1; i<=100; i++){
+        if(i%3 === 0 && i%4 ===0){
+        return i;
+        }
+    }
+}
+console.log(findSmallest());
