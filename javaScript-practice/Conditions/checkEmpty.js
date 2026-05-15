d3b7d769182f328517581acc09b037a908ac5267
AashishhChau <aashishchau012@gmail.com>
2026-05-15 14:02:00 +0545

    Check if a string is empty or not

---
+++ javaScript-practice/Conditions/checkEmpty.js
@@ -0,0 +1,9 @@
+// Empty String Check: Write an `if-else` statement to check if a string is empty. Print "Empty" if true, 
+// otherwise "Not empty."
+
+let text = 'hh'
+if (text === ''){
+  console.log("Empty");
+}else{
+  console.log("Not Empty");
+}
