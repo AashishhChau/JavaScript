a7f353d35bd521f067d018559881acaf893d1b57
AashishhChau <aashishchau012@gmail.com>
2026-05-15 14:16:53 +0545

    Check if a string starts with the letter A or not

---
+++ javaScript-practice/Conditions/stringStartWithA.js
@@ -0,0 +1,9 @@
+// String Starts with A: Write an `if-else` statement to check if a string starts with the letter "A". Print 
+// "Starts with A" if true, otherwise "Does not start with A.
+
+let text = "ashish";
+if(text.toLowerCase().startsWith("a")){
+  console.log("Start With A");
+}else{
+  console.log("Not start with A");
+}
