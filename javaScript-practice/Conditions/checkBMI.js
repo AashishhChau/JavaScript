7757ae17515737415f78c31829530f7fd0927879
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:26:34 +0545

    Check if a person's BMI is in the healthy range or not

---
+++ javaScript-practice/Conditions/checkBMI.js
@@ -0,0 +1,9 @@
+// BMI Check: Write an `if-else` statement to check if a person’s Body Mass Index (BMI) is in the 
+// healthy range (18.5 to 24.9). Print "Healthy" if true, otherwise "Not healthy." 
+
+let range = 10;
+if(range >= 18.5 && range <= 24.9){
+  console.log("Healthy");
+}else{
+  console.log("Not healthy");
+}
