041b2ca00e1aa10b6b9795901dae25008fb1e85d
AashishhChau <aashishchau012@gmail.com>
2026-05-15 14:21:42 +0545

    Check if a string contains a space or not

---
+++ javaScript-practice/Conditions/checkForSpace.js
@@ -0,0 +1,9 @@
+// Check for Space: Write an `if-else` statement to check if a string contains a space. Print "Contains 
+// space" if true, otherwise "No space."
+
+let text = "Aashish Tharu";
+if(text.includes(" ")){
+  console.log("Contains space");
+}else{
+  console.log("No space");
+}
