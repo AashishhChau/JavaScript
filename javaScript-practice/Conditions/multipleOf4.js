4c399b97edc78ab0387b9e559fbe9adc63b6d28a
AashishhChau <aashishchau012@gmail.com>
2026-05-15 14:05:32 +0545

    Check if a number is a multiple of 4 or not.

---
+++ javaScript-practice/Conditions/multipleOf4.js
@@ -0,0 +1,9 @@
+// Multiple of 4: Write an `if-else` statement to check if a number is a multiple of 4. Print "Multiple of 4" 
+// if true, otherwise "Not a multiple of 4."
+
+let num = 48;
+if(num%4 === 0){
+  console.log("multiple of 4");
+}else{
+  console.log("Not a multiple o 4");
+}
