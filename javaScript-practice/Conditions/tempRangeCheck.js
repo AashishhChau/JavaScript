f7c5e3d3d8396b82bf9453cfa277f36f826308ad
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:54:17 +0545

    Check if a temperature is between 15 and 25 degresss Celsius

---
+++ javaScript-practice/Conditions/tempRangeCheck.js
@@ -0,0 +1,9 @@
+// Temperature Range Check: Write an `if-else` statement to check if a temperature is between 15 and 25 
+// degrees Celsius. Print "Comfortable" if true, otherwise "Uncomfortable."
+
+let temp = 10;
+if(temp >= 15 && temp <= 25){
+  console.log("Comfortable");
+}else{
+  console.log("Uncomfortable");
+}
