02f167acf2d84472a6c685b16ffb0199c73beed3
AashishhChau <aashishchau012@gmail.com>
2026-05-15 13:54:13 +0545

    Check if a number is Zero or not.

---
+++ javaScript-practice/Conditions/checkZero.js
@@ -0,0 +1,9 @@
+// Check for Zero: Write an `if-else` statement to check if a number is zero. Print "Zero" if true, 
+// otherwise "Not zero."
+
+let num = 4;
+if(num === 0){
+  console.log("Zero");
+}else{
+  console.log("Not Zero");
+}
