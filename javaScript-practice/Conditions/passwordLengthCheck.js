7f3dda7c4943261edf5dc7ae341ec7dc0f16df6c
AashishhChau <aashishchau012@gmail.com>
2026-05-15 13:50:33 +0545

    Check if a passwor is at least 8 characters long or not.

---
+++ javaScript-practice/Conditions/passwordLengthCheck.js
@@ -0,0 +1,9 @@
+// Password Length Check: Write an `if-else` statement to check if a password is at least 8 characters 
+// long. Print "Valid password" if true, otherwise "Password too short." 
+
+let password = "aa12";
+if(password.length>8){
+  console.log("Valid password");
+}else{
+  console.log("Not a Valid password");
+}
