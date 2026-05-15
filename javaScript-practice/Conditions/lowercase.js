dfb09cf8473212fde62eed04e641774361f8c4d9
AashishhChau <aashishchau012@gmail.com>
2026-05-15 13:30:44 +0545

    Check ifa number is a  Single digit number

---
+++ javaScript-practice/Conditions/lowercase.js
@@ -0,0 +1,9 @@
+// Letter Case Check: Write an `if-else` statement to check if a character is lowercase. Print "Lowercase" 
+// if true, otherwise "Uppercase."
+
+let char = "a";
+if(char ===char.toLowerCase()){
+  console.log("Lowercase");
+}else{
+  console.log("uppercase")
+}
---
+++ javaScript-practice/Conditions/singleDigit.js
@@ -0,0 +1,9 @@
+// 30. Number Check: Write an `if-else` statement to check if a number is a single-digit number. Print 
+// "Single-digit" if true, otherwise "More than one digit." 
+
+let num = 7;
+if(num <= 9 && num >= -9){
+    console.log("Single Digit");
+}else{
+    console.log("More than one digit");
+}
