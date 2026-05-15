48e8b36e5403cb56bbb985845ea6870bf55c010e
AashishhChau <aashishchau012@gmail.com>
2026-05-15 13:35:59 +0545

    Check if a given number is divisible by 10 or not

---
+++ javaScript-practice/Conditions/divisibleBY10.js
@@ -0,0 +1,9 @@
+//  Number Check: Write an `if-else` statement to check if a number is a single-digit number. Print 
+// "Single-digit" if true, otherwise "More than one digit."
+
+let num = 15;
+if(num%10 === 0){
+  console.log("Divisible by 10");
+}else{
+  console.log("Not divisible by 10");
+}
