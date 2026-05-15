6bcd304e2435658ebbf7469e3d83d54e89c0ba80
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:39:55 +0545

    Check it a string end with an exclamation mark(!)

---
+++ javaScript-practice/Conditions/checkForExclamation.js
@@ -0,0 +1,9 @@
+// Check for Exclamation: Write an `if-else` statement to check if a string ends with an exclamation 
+// mark (!). Print "Exclamation mark" if true, otherwise "No exclamation mark." 
+
+let text = "Hi!";
+if(text.endsWith("!")){
+  console.log("Exclamation mark");
+}else{
+  console.log("No exclamation mark");
+}
