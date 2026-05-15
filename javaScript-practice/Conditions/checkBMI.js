39ee5e70c1df752c0ffe50af2fa4801671e02875
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:29:43 +0545

    Check if a numbre is odd or even

--- javaScript-practice/Conditions/checkBMI.js
+++ javaScript-practice/Conditions/checkBMI.js
@@ -1,8 +1,8 @@
 // BMI Check: Write an `if-else` statement to check if a person’s Body Mass Index (BMI) is in the 
 // healthy range (18.5 to 24.9). Print "Healthy" if true, otherwise "Not healthy." 
 
-let range = 10;
-if(range >= 18.5 && range <= 24.9){
+let bmi = 10;
+if(bmi >= 18.5 && bmi <= 24.9){
   console.log("Healthy");
 }else{
   console.log("Not healthy");
---
+++ javaScript-practice/Conditions/checkOddNum.js
@@ -0,0 +1,8 @@
+// Check Odd Number: Write an `if-else` statement to check if a number is odd. Print "Odd" if true, otherwise "Even." 
+
+let num = 15;
+if(num%2 !==0){
+    console.log("Odd numbers");
+}else{
+    console.log("Even numbers");
+}
