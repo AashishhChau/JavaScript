c0bd8d2fb0bac055c37641b98d7aa73b87a57403
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:46:21 +0545

    Check if today is a weekday or weekend

---
+++ javaScript-practice/Conditions/dayCheck.js
@@ -0,0 +1,17 @@
+// Day Check: Write an `if-else` statement? to check if today is a weekday (Monday to Friday). Print 
+// "Weekday" if true, otherwise "Weekend."
+
+
+let day = "Sunday";
+if(
+  day === "Monday" ||
+  day === "Tuesday" ||
+  day === "Wednesday" ||
+  day === "Thursday" ||
+  day === "Friday"
+  ){
+    console.log("Weekday");
+  }else{
+    console.log("Weekend");
+  }
+  
