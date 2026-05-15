763078238379d44dd5e8376e2c02bbb9e9b282fb
AashishhChau <aashishchau012@gmail.com>
2026-05-15 22:17:13 +0545

    Check if a temperature in celsius is above degrees.

---
+++ javaScript-practice/Conditions/tempConversion.js
@@ -0,0 +1,9 @@
+// Temperature Conversion: Write an `if-else` statement to check if a temperature in Celsius is above 100 
+// degrees. Print "Above boiling point" if true, otherwise "Below boiling point."
+
+let temp = 20;
+if(temp>100){
+  console.log("Above boiling point");
+}else{
+  console.log("Below boiling point");
+}
