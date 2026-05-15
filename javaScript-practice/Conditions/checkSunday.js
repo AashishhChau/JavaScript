f47a4b8d5f6bdad4c64645ddca351b3a9a790ba8
AashishhChau <aashishchau012@gmail.com>
2026-05-15 14:11:19 +0545

    Check if a given day is Sunday or not not.

---
+++ javaScript-practice/Conditions/checkSunday.js
@@ -0,0 +1,10 @@
+// Day of the Week: Write an `if-else` statement to check if a given day is Sunday. Print "Sunday" if true, 
+// otherwise "Not Sunday."
+
+let day = "Sunday";
+
+if (day.toLowerCase() === "sunday") {
+  console.log("Sunday");
+} else {
+  console.log("Not Sunday");
+}
