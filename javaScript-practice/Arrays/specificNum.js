6cd80d04c4d0d09400577a1359e595331b712b44
AashishhChau <aashishchau012@gmail.com>
2026-05-19 22:20:12 +0545

    Check if the number 7 is present in the array or not

---
+++ javaScript-practice/Arrays/specificNum.js
@@ -0,0 +1,9 @@
+// - Write a code to check if the number `7` is present in the array `[5, 6, 7, 8]` and print "Found" or "Not Found".
+
+let array = [5, 6, 7, 8];
+
+if(array.includes(7)){
+    console.log("Found");
+}else{
+    console.log("Not found");
+}
