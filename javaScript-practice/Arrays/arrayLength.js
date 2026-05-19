d2f0cc38e15ea0893e75990f05367bc02b76849e
AashishhChau <aashishchau012@gmail.com>
2026-05-19 22:46:37 +0545

    Print the length of the array

---
+++ javaScript-practice/Arrays/arrayLength.js
@@ -0,0 +1,5 @@
+//  Write a code to print the length of the array `[9, 7, 5, 3, 1]`.
+// Write a code to print the length of the array `[9, 7, 5, 3, 1]`.
+
+let array =[9, 7, 5, 3, 1];
+console.log(array.length);
