2f514cd4ecb2f781ac71d2cfae2687fe3b57dad2
AashishhChau <aashishchau012@gmail.com>
2026-05-19 22:32:58 +0545

    Create a new array containing the squares of the element from the array

---
+++ javaScript-practice/Arrays/square.js
@@ -0,0 +1,6 @@
+// Write a code to create a new array containing the squares of the elements from the array `[1, 2, 3]`.
+
+let array = [1, 2, 3];
+
+let squaresArr = array.map(num => num*num);
+console.log(squaresArr);
