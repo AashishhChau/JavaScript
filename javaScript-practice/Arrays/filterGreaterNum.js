6507f741a8bbcfa8c70b9c0d427c46f95370e78e
AashishhChau <aashishchau012@gmail.com>
2026-05-19 22:36:37 +0545

    Filter and print numbers greater than 10 from the array

---
+++ javaScript-practice/Arrays/filterGreaterNum.js
@@ -0,0 +1,6 @@
+// Write a code to filter and print numbers greater than `10` from the array `[5, 12, 7, 14, 3]`.
+
+let array = [5, 12, 7, 14, 3];
+
+let greaterNum = array.filter(num => num > 10);
+console.log(greaterNum);
